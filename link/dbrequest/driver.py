2acd7ce0ad13fcc948328c15a2a12f0c1f95e56e
David Delassus <david.jose.delassus@gmail.com>
2016-06-09 14:59:47 +0200

    :add: Model and Cursor

--- link/dbrequest/driver.py
+++ link/dbrequest/driver.py
@@ -2,13 +2,8 @@
 
 from b3j0f.conf import Configurable, category
 from link.middleware.connectable import ConnectableMiddleware
+from link.dbrequest.model import Model, Cursor
 from link.dbrequest import CONF_BASE_PATH
-
-from link.dbrequest.comparison import C
-from link.dbrequest.assignment import A
-from link.dbrequest.ast import AST
-
-import json
 
 
 @Configurable(
@@ -25,6 +20,9 @@ class Driver(ConnectableMiddleware):
     QUERY_UPDATE = 'update'
     QUERY_DELETE = 'delete'
 
+    model_class = Model
+    cursor_class = Cursor
+
     def _process_query(self, conn, query):
         raise NotImplementedError()
 
@@ -40,7 +38,7 @@ class Driver(ConnectableMiddleware):
             'update': ast
         })
 
-        return Model(self, result)
+        return self.model_class(self, result)
 
     def find_elements(self, ast):
         result = self._process_query(self.conn, {
@@ -48,10 +46,7 @@ class Driver(ConnectableMiddleware):
             'filter': ast
         })
 
-        return [
-            Model(self, item)
-            for item in result
-        ]
+        return self.cursor_class(self, result)
 
     def update_elements(self, filter_ast, update_ast):
         return self._process_query(self.conn, {
@@ -65,80 +60,3 @@ class Driver(ConnectableMiddleware):
             'type': Driver.QUERY_DELETE,
             'filter': ast
         })
-
-
-class Model(object):
-    def __init__(self, driver, data, *args, **kwargs):
-        super(Model, self).__init__(*args, **kwargs)
-
-        self.driver = driver
-        self.data = data
-
-    def __str__(self):
-        return json.dumps(self.data)
-
-    def __repr__(self):
-        return 'Model({0})'.format(json.dumps(self.data))
-
-    def _get_filter(self):
-        condition = None
-
-        for key, val in self.data.items():
-            if condition is None:
-                condition = C(key) == val
-
-            else:
-                condition = condition & (C(key) == val)
-
-        return condition
-
-    def _get_update(self):
-        return [
-            A(key, val).get_ast()
-            for key, val in self.data.items()
-        ]
-
-    def save(self):
-        assignments = self._get_update()
-        self.result = self.driver.put_element(assignments)
-
-    def delete(self):
-        condition = self._get_filter()
-
-        self.driver.remove_elements([
-            AST('filter', condition.get_ast())
-        ])
-
-    def __getitem__(self, prop):
-        return self.data[prop]
-
-    def __setitem__(self, prop, val):
-        self.data[prop] = val
-
-    def __delitem__(self, prop):
-        del self.data[prop]
-
-    def __getattribute__(self, prop):
-        if prop in ['data', 'driver', '__dict__']:
-            return super(Model, self).__getattribute__(prop)
-
-        if prop not in self.data:
-            raise AttributeError(
-                'No attribute "{0}" found in data'.format(prop)
-            )
-
-        return self.data[prop]
-
-    def __setattr__(self, prop, val):
-        if prop in ['data', 'driver', '__dict__']:
-            super(Model, self).__setattr__(prop, val)
-
-        else:
-            self.data[prop] = val
-
-    def __delattr__(self, prop):
-        if prop in ['data', 'driver', '__dict__']:
-            super(Model, self).__delattr__(prop)
-
-        else:
-            del self.data[prop]
