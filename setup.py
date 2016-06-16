2312868099c023eb527ab47c9baa94a2a2bca40a
David Delassus <david.jose.delassus@gmail.com>
2016-06-16 15:28:35 +0200

    :fix: middleware dependency

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ AUTHOR_EMAIL = 'david.jose.delassus@gmail.com'
 LICENSE = 'MIT'
 REQUIREMENTS = [
     'b3j0f.conf>=0.3.18',
-    'link.middleware>=0.11',
+    'link.middleware>=0.12',
     'six>=1.10.0',
     'mock>=2.0.0'
 ]
